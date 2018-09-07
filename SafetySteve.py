2a5ed67acf567c59f60273e4c863484cbc8ad096
GE0NE <seantaylor951753@yahoo.ca>
2018-09-07 00:39:28 -0400

    Fixed shebang line

--- SafetySteve.py
+++ SafetySteve.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*- 
 import os                                                                                                       # import all sorts of shit
 import sys                                                                                                      # like the OS and the System
